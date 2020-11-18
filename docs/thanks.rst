f778e9f116a757cddae309b685a52e9491ed8e96
Luc Saffre <luc.saffre@gmail.com>
2020-11-19 00:30:55 +0200

    add other versions of sepa pain xsd

--- docs/thanks.rst
+++ docs/thanks.rst
@@ -4,9 +4,11 @@ Thanks to
 Publications
 ------------
 
-Here is a list of other publications who are at least partially being 
+Here is a list of other publications who are at least partially being
 distributed with etgen.
 
 - :srcref:`/etgen/html.py` is a modified copy of the
   ElementTree toolkit, Copyright (c) 1999-2004 by Fredrik Lundh.
 
+- XSD files for :mod:`etgen.sepa.validate` are from
+  https://www.iso20022.org/catalogue-messages
--- etgen/sepa/validate.py
+++ etgen/sepa/validate.py
@@ -1,5 +1,5 @@
 # -*- coding: UTF-8 -*-
-# Copyright 2013-2018 Rumma & Ko Ltd
+# Copyright 2013-2020 Rumma & Ko Ltd
 # License: BSD (see file COPYING for details)
 
 
@@ -13,7 +13,14 @@ Usage::
 Arguments:
 
 XMLFILE : the name of the xml file to validate, or '-' to read from stdin
+
+
+- What is a SEPA Payments Initiation?
+  https://www.febelfin.be/sites/default/files/2019-04/standard-credit_transfer-xml-v32-en_0.pdf
+- XSD files are from https://www.iso20022.org/catalogue-messages
+
 """
+
 
 import sys
 from os.path import join, dirname
@@ -23,6 +30,7 @@ def validate_pain001(xmlfile):
     # doc = etree.parse(open(xmlfile))
     doc = etree.parse(xmlfile)
     xsdfile = join(dirname(__file__), 'XSD', 'pain.001.001.02.xsd')
+    # xsdfile = join(dirname(__file__), 'XSD', 'pain.001.001.03.xsd')
     # xsd = etree.XMLSchema(etree.parse(open(xsdfile)))
     xsd = etree.XMLSchema(etree.parse(xsdfile))
     xsd.assertValid(doc)
